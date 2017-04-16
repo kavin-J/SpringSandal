42b839e1bedee2b88d2e973b98a617881dc993f4
nike11 <2078724466@qq.com>
2017-04-16 11:19:40 +0800

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	//测试一下
 	@Test
 	public void contextLoads() {
 	}
